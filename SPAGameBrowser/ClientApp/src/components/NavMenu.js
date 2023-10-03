293f3bb0f0d0686d6c4d0035f0f5fe2a0c4a0777
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-03 19:27:27 +0200

    styles update

--- SPAGameBrowser/ClientApp/src/components/NavMenu.js
+++ SPAGameBrowser/ClientApp/src/components/NavMenu.js
@@ -26,7 +26,8 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">SPAGameBrowser</NavbarBrand>
+                <img src={process.env.PUBLIC_URL + '/puzzle.png'} alt="Logo" style={{ width: '30px', height: '30px', paddingRight: '3px' }} />
+                <NavbarBrand tag={Link} to="/">Wordle</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
