5748c23c19b7dbc22cf1b8d6f75bc343dc6beb7c
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-06 16:34:07 +0200

    new entities setup

--- SPAGameBrowser/Data/ApplicationDbContext.cs
+++ SPAGameBrowser/Data/ApplicationDbContext.cs
@@ -8,18 +8,24 @@ namespace SPAGameBrowser.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public DbSet<Word>? Words { get; set; }
+        public DbSet<UserScoreBoard>? UserScores { get; set; }
+
         public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
             : base(options, operationalStoreOptions)
         {
 
         }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<ApplicationUser>()
                 .HasIndex(u => u.Nickname)
+                .IsUnique();
+
+            modelBuilder.Entity<Word>()
+                .HasIndex(u => u.WordName)
                 .IsUnique();
         }
     }
