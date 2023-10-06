b7391800f24936035901eb092e7d244385ceb415
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-06 15:41:30 +0200

    dataAnnotation setup

--- SPAGameBrowser/Models/Word.cs
+++ SPAGameBrowser/Models/Word.cs
@@ -1,8 +1,18 @@
-﻿namespace SPAGameBrowser.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Xml.Linq;
+
+namespace SPAGameBrowser.Models
 {
     public class Word
     {
+        [Key]
         public int WordId { get; set; }
+
+        [Required, NotNull]
+        [Display (Name = "Word")]
+        [StringLength(5, MinimumLength = 5, ErrorMessage = "5-letter words only.")]
         public string? WordName { get; set; }
     }
 }
