30e04377713206ca48e4c8a755bdb21cdd74aa39
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-06 16:02:35 +0200

    DataAnnotation setup

--- SPAGameBrowser/Models/UserScoreBoard.cs
+++ SPAGameBrowser/Models/UserScoreBoard.cs
@@ -5,13 +5,31 @@ namespace SPAGameBrowser.Models
 {
     public class UserScoreBoard
     {
+        [Key]
         public int UserScoreBoardId { get; set; }
+
+        [Required, NotNull]
         public int FkUserId { get; set; }
-        public string? FkWord { get; set; }
+
+        [Required, NotNull]
+        public string? FkWordId { get; set; }
+
+        [Required, NotNull]
+        [Display(Name = "IP Address")]
         public int IpAddress { get; set; }
-        public int TimeGuesses { get; set; }
-        public bool IsGuessesDone { get; set; }
+
+        [Required, NotNull]
+        [Display(Name = "Time Guesses")]
+        public int TimeGuesses { get; set; } = 0;
+
+        [Required, NotNull]
+        public bool IsGuessesDone { get; set; } = false;
+
+        [Required, NotNull]
         public DateTime? Started_At { get; set; }
+
+        [Required]
         public DateTime? Finished_At { get; set; }
+
     }
 }
