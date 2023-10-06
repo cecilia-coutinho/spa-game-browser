e9bdc7136fc308d7b2963fc133a347c88166071f
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-06 15:59:08 +0200

    ScoreBoardModel setup

---
+++ SPAGameBrowser/Models/UserScoreBoard.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SPAGameBrowser.Models
+{
+    public class UserScoreBoard
+    {
+        public int UserScoreBoardId { get; set; }
+        public int FkUserId { get; set; }
+        public string? FkWord { get; set; }
+        public int IpAddress { get; set; }
+        public int TimeGuesses { get; set; }
+        public bool IsGuessesDone { get; set; }
+        public DateTime? Started_At { get; set; }
+        public DateTime? Finished_At { get; set; }
+    }
+}
