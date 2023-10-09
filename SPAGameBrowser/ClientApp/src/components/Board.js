85b16a5c7b71c16d4d2b398c9dddcdc642fe74b1
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-09 17:00:10 +0200

    component 1st setup

---
+++ SPAGameBrowser/ClientApp/src/components/Board.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class Board extends Component {
+    static displayName = Board.name;
+
+    render() {
+        return (
+            <div>
+                Board
+            </div>
+        );
+    }
+}
---
+++ SPAGameBrowser/ClientApp/src/components/Keyboard.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class Keyboard extends Component {
+    static displayName = Keyboard.name;
+
+    render() {
+        return (
+            <div>
+                Keyboard
+            </div>
+        );
+    }
+}
