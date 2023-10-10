00177709080848066fae41fbdbd6438fc3cf0929
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-10 17:41:44 +0200

    Board/Keyboard refactoring

--- SPAGameBrowser/ClientApp/src/components/GamePage.js
+++ SPAGameBrowser/ClientApp/src/components/GamePage.js
@@ -9,16 +9,47 @@ export const gameContext = createContext();
 
 const GamePage = () => {
     const [board, setBoard] = useState(boardDefault);
-    console.log('board:', board);
+    //console.log('board:', board);
+    const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
 
-    const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0});
+    const onSelectLetter = (keyVal) => {
+        if (currAttempt.letterPos > 4) return;
+
+        const newBoard = [...board];
+        newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+        setBoard(newBoard);
+        setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+    }
+
+    const onDelete = () => {
+        if (currAttempt.letterPos === 0) return;
+
+        const newBoard = [...board];
+        newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+        setBoard(newBoard);
+        setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 })
+    }
+
+    const onEnter = () => {
+        if (currAttempt.letterPos !== 5) return;
+
+        setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 })
+    }
 
     return (
         <div>
             <h1>Wordle Clone</h1>
             <p>Coming soon</p>
 
-            <gameContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt }}>
+            <gameContext.Provider value={{
+                board,
+                setBoard,
+                currAttempt,
+                setCurrAttempt,
+                onSelectLetter,
+                onDelete,
+                onEnter
+            }}>
                 <div className="game">
                 <Board />
                 <Keyboard />
--- SPAGameBrowser/ClientApp/src/components/Key.js
+++ SPAGameBrowser/ClientApp/src/components/Key.js
@@ -2,32 +2,27 @@
 import { gameContext } from "./GamePage"
 
 const Key = ({ keyVal, bigKey }) => {
-    const { board, setBoard, currAttempt, setCurrAttempt } = useContext(gameContext);
+    const {
+        board,
+        setBoard,
+        currAttempt,
+        setCurrAttempt,
+        onSelectLetter,
+        onDelete,
+        onEnter } = useContext(gameContext);
 
     const selectLetter = () => {
         if (keyVal === "ENTER")
         {
-            if (currAttempt.letterPos !== 5) return;
-
-            setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0})
+            onEnter()
         }
         else if (keyVal === "DELETE")
         {
-            if (currAttempt.letterPos === 0) return;
-
-            const newBoard = [...board];
-            newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
-            setBoard(newBoard);
-            setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos -1 })
+            onDelete()
         }
         else
         {
-            if (currAttempt.letterPos > 4) return;
-
-            const newBoard = [...board];
-            newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
-            setBoard(newBoard);
-            setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+            onSelectLetter(keyVal)
         }
     }
 
