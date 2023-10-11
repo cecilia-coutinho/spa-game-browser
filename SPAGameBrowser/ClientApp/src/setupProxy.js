7c683baf34ee041e460870b0576716f3db55b654
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-11 18:05:32 +0200

    context update & onError setup

--- SPAGameBrowser/ClientApp/src/setupProxy.js
+++ SPAGameBrowser/ClientApp/src/setupProxy.js
@@ -5,7 +5,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:47876';
 
 const context =  [
-  "/weatherforecast",
+  "/api",
   "/_configuration",
   "/.well-known",
   "/Identity",
@@ -14,11 +14,18 @@ const context =  [
   "/_framework"
 ];
 
+const onError = (err, req, resp, target) => {
+    console.error(`${err.message}`);
+}
+
 module.exports = function(app) {
   const appProxy = createProxyMiddleware(context, {
     proxyTimeout: 10000,
     target: target,
+    onError: onError,
     secure: false,
+    // Uncomment this line to add support for proxying websockets
+    //ws: true, 
     headers: {
       Connection: 'Keep-Alive'
     }
