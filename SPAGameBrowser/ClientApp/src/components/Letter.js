ea7de3446801215cc6098ef144ba66d0da7a2460
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-11 15:30:49 +0200

    colorLetters attempts setup

--- SPAGameBrowser/ClientApp/src/components/Letter.js
+++ SPAGameBrowser/ClientApp/src/components/Letter.js
@@ -3,16 +3,22 @@ import { gameContext } from "./GamePage";
 import '../custom.css';
 
 const Letter = ({ letterPosition, attemptValue }) => {
+    //console.log('letterPosition:', letterPosition);
+    //console.log('attemptValue:', attemptValue);
+    const { board, correctWord, currAttempt } = useContext(gameContext);
+    const letter = board[attemptValue][letterPosition];
 
-    console.log('letterPosition:', letterPosition);
-    console.log('attemptValue:', attemptValue);
+    const correctLetter = correctWord.toUpperCase()[letterPosition] === letter;
 
+    const almostLetter = !correctLetter && letter !== ""
+        && correctWord.toUpperCase().includes(letter);
 
-    const { board } = useContext(gameContext);
-    const letter = board[attemptValue][letterPosition]
+    const letterState = currAttempt.attempt > attemptValue &&
+        (correctLetter ? "correct" : almostLetter ? "almost" : "error");
+
 
     return (
-        <div className="letter">
+        <div className="letter" id={letterState}>
             { letter }
         </div>
     );
