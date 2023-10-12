691207f99a2e8bbff4db65676bf827f546977ad8
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-12 16:56:27 +0200

    fetch words, split it and transform to a set

--- SPAGameBrowser/ClientApp/src/components/Words.js
+++ SPAGameBrowser/ClientApp/src/components/Words.js
@@ -1,4 +1,6 @@
-﻿export const boardDefault = [
+﻿import authService from './api-authorization/AuthorizeService';
+
+export const boardDefault = [
     ["", "", "", "", ""],
     ["", "", "", "", ""],
     ["", "", "", "", ""],
@@ -6,3 +8,34 @@
     ["", "", "", "", ""],
     ["", "", "", "", ""],
 ];
+
+export const getWord = async () => {
+    try {
+        const token = await authService.getAccessToken();
+        const response = await fetch(`api/Words`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        //console.log(result)
+
+        if (!Array.isArray(result)) {
+            throw new Error('Unexpected response format. Expected an array.');
+        }
+
+        const wordArr = Array.from(result).map(item => item.wordName.split('\n'));
+        const flattenedArray = [].concat(...wordArr);
+        const wordSet = new Set(flattenedArray);
+        return wordSet;
+    } catch (error) {
+        console.error('Error: ', error);
+        throw error; 
+    }
+};
