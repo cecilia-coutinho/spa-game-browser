fd0137d8c825109d364ee62c47d64cd5dd9f1abe
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-16 15:47:27 +0200

    caching setup

--- SPAGameBrowser/Controllers/WordsController.cs
+++ SPAGameBrowser/Controllers/WordsController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using SPAGameBrowser.Data;
 using SPAGameBrowser.Models;
+using System.Diagnostics;
 
 namespace SPAGameBrowser.Controllers
 {
@@ -12,20 +14,60 @@ namespace SPAGameBrowser.Controllers
     public class WordsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        public WordsController(ApplicationDbContext context)
+        private readonly IMemoryCache _cache;
+        private readonly string cacheKey = "wordsCacheKey";
+        private readonly ILogger<WordsController> _logger;
+        public WordsController(ApplicationDbContext context, IMemoryCache cache, ILogger<WordsController> logger)
         {
             _context = context;
+            _cache = cache;
+            _logger = logger;
         }
 
         // GET: api/Words
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Word>>> GetWords()
         {
-            if (_context.Words == null)
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            if (_cache.TryGetValue(cacheKey, out IEnumerable<Word> words))
             {
-                return NotFound();
+                _logger.Log(LogLevel.Information, "Words found in cache");
             }
-            return await _context.Words.ToListAsync();
+            else
+            {
+                _logger.Log(LogLevel.Information, "Words NOT found in cache. Loading.");
+
+                if (_context.Words == null)
+                {
+                    return NotFound();
+                }
+
+                words = await _context.Words.ToListAsync();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromDays(1))
+                    .SetAbsoluteExpiration(TimeSpan.FromDays(7))
+                    .SetPriority(CacheItemPriority.Normal);
+
+                _cache.Set(cacheKey, words, cacheEntryOptions);
+            }
+
+            stopwatch.Stop();
+
+            _logger.Log(LogLevel.Information, "PassedTime: " + stopwatch.ElapsedMilliseconds);
+
+            return Ok(words);
         }
+
+
+        //public IActionResult ClearCache()
+        //{
+        //    _cache.Remove(cacheKey);
+        //    _logger.Log(LogLevel.Information, "Cleared cache");
+
+        //    return Ok();
+        //}
     }
 }
