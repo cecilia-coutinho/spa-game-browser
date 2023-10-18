dcb37b2f1e14b0fd42ee411bc6045751ceff23ad
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-18 11:44:52 +0200

    replace DateTime to String for test purposes

--- SPAGameBrowser/Data/Migrations/20231017211936_UserScoreUpdate4.Designer.cs
+++ SPAGameBrowser/Data/Migrations/20231017211936_UserScoreUpdate4.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SPAGameBrowser.Data;
 
@@ -11,9 +12,10 @@ using SPAGameBrowser.Data;
 namespace SPAGameBrowser.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231017211936_UserScoreUpdate4")]
+    partial class UserScoreUpdate4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SPAGameBrowser/Data/Migrations/20231017211936_UserScoreUpdate4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SPAGameBrowser.Data.Migrations
+{
+    public partial class UserScoreUpdate4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Started_At",
+                table: "UserScores",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Finished_At",
+                table: "UserScores",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Started_At",
+                table: "UserScores",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Finished_At",
+                table: "UserScores",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
