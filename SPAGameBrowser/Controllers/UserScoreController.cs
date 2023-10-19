3723ab301d18917ae7203d8a49b6246a5c47eba0
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-19 17:16:24 +0200

    Get user statistics

--- SPAGameBrowser/Controllers/UserScoreController.cs
+++ SPAGameBrowser/Controllers/UserScoreController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SPAGameBrowser.Data;
@@ -20,37 +21,59 @@ namespace SPAGameBrowser.Controllers
             _context = context;
         }
 
-        // GET: api/UsersScore
+        // GET: api/UserScore
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<UserScore>>> GetAllUsersScores()
+        public async Task<ActionResult<IEnumerable<UserScore>>> GetUserScores()
         {
-            if (_context.UserScores == null)
+            if (_context.UserScores == null || _context.Users == null)
             {
-                return NotFound();
+                return Problem("Entity is null.");
             }
-            return await _context.UserScores.ToListAsync();
-        }
 
-        // GET: api/UserScore/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<IEnumerable<UserScore>>> GetUserScores(string id)
-        {
-            if (_context.UserScores == null)
-            {
-                return NotFound();
-            }
-            var userScoreBoard = await _context.UserScores
-                .Where(us => us.FkUserId == id)
-                .OrderByDescending(us => us.Finished_At)
+            var result = await _context.UserScores
+                .Join(_context.Users,
+                    h => h.FkUserId,
+                    u => u.Id,
+                    (h, u) => new { UserScore = h, User = u }
+                )
+                .GroupBy(joined => joined.User.Nickname)
+                .Select(group => new ScoreViewModel()
+                {
+                    Name = group.Key,
+
+                    TotalGamesPlayed = group.Count(),
+
+                    TotalGamesWon = group.Count(h => h.UserScore.IsGuessed == true),
+
+                    WinningPercentage = Math.Round((double)group.Count(h => h.UserScore.IsGuessed == true) * 100.0 / group.Count(), 2),
+
+                    AverageGuessesPerGame = group.Average(h => h.UserScore.Attempts)
+                })
                 .ToListAsync();
 
-            if (userScoreBoard == null)
-            {
-                return NotFound();
-            }
-
-            return userScoreBoard;
+            return Ok(result);
         }
+
+        //// GET: api/UserScore/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<IEnumerable<UserScore>>> GetUserScores(string id)
+        //{
+        //    if (_context.UserScores == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var userScoreBoard = await _context.UserScores
+        //        .Where(us => us.FkUserId == id)
+        //        .OrderByDescending(us => us.Finished_At)
+        //        .ToListAsync();
+
+        //    if (userScoreBoard == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return userScoreBoard;
+        //}
 
         // POST: api/UserScore
         [HttpPost]
@@ -75,5 +98,26 @@ namespace SPAGameBrowser.Controllers
 
             return CreatedAtAction("GetUserScore", new { id = userScore.UserScoreId }, userScore);
         }
+        //public async Task<IActionResult> GetDailyScores(string userId)
+        //{
+        //    DateTime today = DateTime.Today;
+
+        //    if (_context.UserScores == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    List<UserScore> dailyScores = await _context.UserScores
+        //        .Where(score => DateTime.Parse(score.Started_At).Date == today && score.IsGuessed)
+        //        .OrderBy(score => score.Attempts)
+        //        .ToListAsync();
+
+        //    var viewModel = new ScoreViewModel
+        //    {
+        //        DailyScores = dailyScores
+        //    };
+
+        //    return Ok(viewModel);
+        //}
     }
 }
