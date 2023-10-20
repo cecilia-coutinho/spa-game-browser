26407628fff5e24935245ab46dc46a95abb687bc
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-20 15:01:10 +0200

    get daily scores

--- SPAGameBrowser/ClientApp/src/components/Highscore.js
+++ SPAGameBrowser/ClientApp/src/components/Highscore.js
@@ -1,63 +1,97 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import authService from './api-authorization/AuthorizeService'
 
-export class Highscore extends Component {
-  static displayName = Highscore.name;
+const Highscore = () => {
+    const [userScores, setUserScores] = useState([]);
+    const [dailyScores, setDailyScores] = useState([]);
 
-  //constructor(props) {
-  //  super(props);
-  //  this.state = { forecasts: [], loading: true };
-  //}
+    const handleGetStatistics = () => {
+        authService.getAccessToken()
+            .then(token => {
+                return axios.get('api/UserScore', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+            })
+            .then(response => {
+                return response.data;
+            })
+            .catch(error => {
+                console.error('Error in GET Request:', error.message);
+                throw error; // Re-throw the error to be caught by the caller
+            });
+    }
 
-  //componentDidMount() {
-  //  this.populateWeatherData();
-  //}
 
-  //static renderForecastsTable(forecasts) {
-  //  return (
-  //    <table className='table table-striped' aria-labelledby="tabelLabel">
-  //      <thead>
-  //        <tr>
-  //          <th>Date</th>
-  //          <th>Temp. (C)</th>
-  //          <th>Temp. (F)</th>
-  //          <th>Summary</th>
-  //        </tr>
-  //      </thead>
-  //      <tbody>
-  //        {forecasts.map(forecast =>
-  //          <tr key={forecast.date}>
-  //            <td>{forecast.date}</td>
-  //            <td>{forecast.temperatureC}</td>
-  //            <td>{forecast.temperatureF}</td>
-  //            <td>{forecast.summary}</td>
-  //          </tr>
-  //        )}
-  //      </tbody>
-  //    </table>
-  //  );
-  //}
+    //useEffect(() => {
+    //    axios.get('api/UserScore')
+    //        .then(response => {
+    //            setUserScores(response.data);
+    //        })
+    //        .catch(error => {
+    //            console.error('Error in GET Request:', error.message);
+    //        });
+    //}, []);
 
-  render() {
-    //let contents = this.state.loading
-    //  ? <p><em>Loading...</em></p>
-    //  : Highscore.renderForecastsTable(this.state.forecasts);
+
+    const handleGetDailyScores = async (dscore) => {
+        return axios.get('api/UserScore/Daily')
+            .then(response => {
+                setDailyScores(response.data);
+            })
+            .catch(error => {
+                console.error('Error in GET Request:', error.message);
+                throw error;
+            });
+    }
+
+    useEffect(() => {
+        handleGetDailyScores();
+    }, []);
+
+    const renderUserScores = (score) => {
+        return handleGetStatistics()
+            .then(userScores => {
+                return (
+                    <div>
+                        <div className="label">Total Games Played: {userScores.totalGamesPlayed}</div>
+                        <div className="label">Total Games Won: {userScores.totalGamesWon}</div>
+                        <div className="label">Winning Percentage: {userScores.winningPercentage} %</div>
+                        <div className="label">Average Guesses Per Game: {userScores.averageGuessesPerGame}</div>
+                    </div>
+                );
+            })
+            .catch(error => {
+                return null;
+            });
+    };
 
     return (
-      <div>
-            <h1 id="tabelLabel" >Highscore</h1>
-            <p>This component demonstrates highscore.</p>
-{/*        {contents}*/}
-      </div>
-    );
-  }
+        <div>
+        <h4 className="label">Hi {userScores.length > 0 ? userScores[0].name : ''}!</h4>
+        <div className="flex-container">
+           {userScores.map((score, index) => (
+                    <div key={index} className="score-container">
+                        <h3>Your Statistics:</h3>
+                        {renderUserScores(score)}
+                    </div>
+                ))}
+            </div>
 
-  //async populateWeatherData() {
-  //  const token = await authService.getAccessToken();
-  //  const response = await fetch('weatherforecast', {
-  //    headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
-  //  });
-  //  const data = await response.json();
-  //  this.setState({ forecasts: data, loading: false });
-  //}
-}
+            <div className="score-container">
+                <h3>Top 10 Daily HighScores:</h3>
+                {dailyScores.map((dscore, index) => (
+                        <div key={ index }>
+                            <p>{dscore.name.toUpperCase()}, {dscore.totalGamesWon} points</p>
+                        </div>
+                ))}
+            </div>
+
+        </div>
+    );
+
+};
+
+export default Highscore;
