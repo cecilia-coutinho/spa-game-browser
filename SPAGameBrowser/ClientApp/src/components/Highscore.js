d45779914a96cafda387ca4c48d018023dcc287b
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-10-20 19:48:30 +0200

    daily leaderboard

--- SPAGameBrowser/ClientApp/src/components/Highscore.js
+++ SPAGameBrowser/ClientApp/src/components/Highscore.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import authService from './api-authorization/AuthorizeService'
 
 const Highscore = () => {
-    const [userScores, setUserScores] = useState([]);
+    const [userStatistics, setUserStatistics] = useState([]);
     const [dailyScores, setDailyScores] = useState([]);
 
     const handleGetStatistics = () => {
@@ -16,25 +16,13 @@ const Highscore = () => {
                 });
             })
             .then(response => {
-                return response.data;
+                setUserStatistics(response.data);
             })
             .catch(error => {
                 console.error('Error in GET Request:', error.message);
-                throw error; // Re-throw the error to be caught by the caller
+                throw error;
             });
     }
-
-
-    //useEffect(() => {
-    //    axios.get('api/UserScore')
-    //        .then(response => {
-    //            setUserScores(response.data);
-    //        })
-    //        .catch(error => {
-    //            console.error('Error in GET Request:', error.message);
-    //        });
-    //}, []);
-
 
     const handleGetDailyScores = async (dscore) => {
         return axios.get('api/UserScore/Daily')
@@ -48,44 +36,27 @@ const Highscore = () => {
     }
 
     useEffect(() => {
+        handleGetStatistics();
         handleGetDailyScores();
     }, []);
 
-    const renderUserScores = (score) => {
-        return handleGetStatistics()
-            .then(userScores => {
-                return (
-                    <div>
-                        <div className="label">Total Games Played: {userScores.totalGamesPlayed}</div>
-                        <div className="label">Total Games Won: {userScores.totalGamesWon}</div>
-                        <div className="label">Winning Percentage: {userScores.winningPercentage} %</div>
-                        <div className="label">Average Guesses Per Game: {userScores.averageGuessesPerGame}</div>
-                    </div>
-                );
-            })
-            .catch(error => {
-                return null;
-            });
-    };
-
     return (
-        <div>
-        <h4 className="label">Hi {userScores.length > 0 ? userScores[0].name : ''}!</h4>
         <div className="flex-container">
-           {userScores.map((score, index) => (
-                    <div key={index} className="score-container">
-                        <h3>Your Statistics:</h3>
-                        {renderUserScores(score)}
-                    </div>
-                ))}
+            <h4 className="label">Hi {userStatistics.name}!</h4>
+            <div className="score-container">
+                <h5>Your Statistics:</h5>
+                <div className="label">Total Games Played: {userStatistics.totalGamesPlayed}</div>
+                <div className="label">Total Games Won: {userStatistics.totalGamesWon}</div>
+                <div className="label">Winning Percentage: {userStatistics.winningPercentage} %</div>
+                <div className="label">Average Guesses Per Game: {userStatistics.averageGuessesPerGame}</div>
             </div>
 
             <div className="score-container">
                 <h3>Top 10 Daily HighScores:</h3>
                 {dailyScores.map((dscore, index) => (
-                        <div key={ index }>
-                            <p>{dscore.name.toUpperCase()}, {dscore.totalGamesWon} points</p>
-                        </div>
+                    <div key={index}>
+                        <p>{dscore.name.toUpperCase()}, {dscore.totalGamesWon} wons.</p>
+                    </div>
                 ))}
             </div>
 
